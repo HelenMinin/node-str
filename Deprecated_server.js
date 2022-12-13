b7ebac39af331aeea60d273594851c6a0a85164b
Minin <helen.msantos@pagar.me>
2022-12-12 21:22:30 -0300

    :lipstick: Separando servidor

--- Deprecated_server.js
+++ Deprecated_server.js
@@ -21,6 +21,7 @@ app.use('/', route)
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 
 console.log('Api rodando na porta ' + port)
 
@@ -59,4 +60,13 @@ function onError(error){
         default:
             throw error;
     }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bird = typeof addr === 'string'
+    ? 'pipe ' + addr 
+    : 'port ' + addr.port;
+
+debug('listening on ' + bird)
 }
